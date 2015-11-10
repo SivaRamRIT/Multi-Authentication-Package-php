a006978f5326612b48393d7864dd983f4687730f
SivaRamRIT <smanijeganathan@scopeinfotechinc.com>
2015-11-10 17:34:25 -0500

    Update code39-example.php

--- code39-example.php
+++ code39-example.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Code39 Login</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap-social.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" href="css/code39-style.css">
 
