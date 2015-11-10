ef0998474df59e1747c540d4cb9f1e3fe4bb339e
SivaRamRIT <smanijeganathan@scopeinfotechinc.com>
2015-11-10 17:34:43 -0500

    Update pdf417-example.php

--- pdf417-example.php
+++ pdf417-example.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Pdf417 Login</title>
-<link rel="stylesheet" type="text/css" href="css/bootstrap-social.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
 </head>
