556163fc74cebf3f62e61f7a31d2760204c20834
SivaRamRIT <smanijeganathan@scopeinfotechinc.com>
2015-11-10 17:06:21 -0500

    Update openid-example.php

--- openid-example.php
+++ openid-example.php
@@ -8,7 +8,7 @@ require 'src/openid.php';
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Title of the document</title>
+<title>OpenID Login</title>
 <link rel="stylesheet" type="text/css" href="css/bootstrap-social.css">
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 
